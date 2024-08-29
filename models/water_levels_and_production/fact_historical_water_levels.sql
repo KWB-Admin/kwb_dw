7e71ce0992017126e710c9c0461db46240f6acad
Michael Johnson <mjohnson@kwb.org>
2024-08-29 10:37:25 -0700

    initial commit

---
+++ models/water_levels_and_production/fact_historical_water_levels.sql
@@ -0,0 +1,62 @@
+{{ config(materialized='table') }}
+
+with mon_well as (
+    select 
+        state_well_num as state_well_number,
+        ground_level,
+        top_screen,
+        bottom_screen
+    from dim_well
+    where well_type = 'Monitoring'
+        and status = 'Active'
+),
+
+metro_data as (
+    select 
+        state_well_number,
+        reading_date,
+        case 
+            when depth_to_water > 400 then null
+            else depth_to_water
+        end as depth_to_water,
+        'metro' as source
+    from stg_metro_2020_2024_water_depth
+),
+
+sems_data as (
+    select 
+        state_well_number,
+        water_levels_read_date as reading_date,
+        case 
+            when dtgw > 400 then null
+            when dtgw < 0 then null
+            else dtgw
+        end as depth_to_water,
+        'sems' as source
+    from stg_historical_water_depth_sems_pre_2020
+    where water_levels_read_date < cast('2019-01-02' as date)
+),
+
+unioned_data as (
+    select * from metro_data
+    union
+    select * from sems_data
+),
+
+final as (
+    select 
+        mon_well.state_well_number,
+        cast(reading_date as date) as reading_date,
+        mon_well.ground_level,
+        depth_to_water,
+        coalesce(mon_well.ground_level - depth_to_water) as ground_water_elevetion,
+        source
+    from mon_well
+        left join unioned_data 
+            on mon_well.state_well_number = unioned_data.state_well_number
+)
+
+select 
+    *
+from final
+order by state_well_number, reading_date desc
