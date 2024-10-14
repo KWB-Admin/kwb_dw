b4a4408d89c500ba361e32149069ae071ba97d06
admin <admin@kwb.org>
2024-10-14 10:08:30 -0700

    Refactored water quality models to reflect current schema

--- models/water_quality/fact_water_quality_data.sql
+++ models/water_quality/fact_water_quality_data.sql
@@ -1,18 +1,57 @@
 {{ config(materialized='table') }}
 
-with wq_data as (
+with hist_digitized_data as (
     select 
         state_well_number,
         sample_id,
         sample_date,
-        test_method,
+        test_method as analysis_method,
         lab,
         analyte,
-        lab_result,
+        lab_result as result,
         units,
-        maximum_contaminant_limit,
-        lab_comment
+        cast(null as real) as min_detectable_limit,
+        cast(maximum_contaminant_limit as real) as max_report_limit
     from {{ ref('stg_historical_water_quality_lab_results') }}
+),
+
+bsk_results as (
+    select 
+        state_well_number,
+        sample_id,
+        sample_date,
+        analysis_method,
+        'BSK' as lab,
+        analyte,
+        result,
+        units,
+        min_detectable_limit,
+        max_report_limit
+    from {{ ref('stg_bsk_lab_results') }}
+),
+
+eurofins_pdf_results as (
+    select 
+        state_well_number,
+        null as sample_id,
+        sample_date,
+        null as analysis_method,
+        'Eurofins' as lab,
+        analyte,
+        result,
+        units,
+        min_detectable_limit,
+        max_report_limit
+        
+    from {{ ref('stg_eurofins_lab_results_from_pdf') }}
+),
+
+wq_data as (
+    select * from hist_digitized_data
+    union
+    select * from bsk_results
+    union
+    select * from eurofins_pdf_results
 ),
 
 results_recast as (
@@ -20,32 +59,50 @@ results_recast as (
         state_well_number,
         sample_id,
         sample_date,
-        test_method,
+        analysis_method,
         lab,
         analyte,
-        lab_result,
+        result,
         units,
-        maximum_contaminant_limit,
-        lab_comment,
+        min_detectable_limit,
+        max_report_limit,
         case
-            when lab_result = 'ND' then 0.0
-            when lab_result like '<%' then cast(trim(translate(lab_result,'<','')) as real)
-            when lab_result = 'No Obs Odor' then null
-            else cast(lab_result as real)
+            when result = 'ND' then 0.0
+            when result like '<%' then cast(trim(translate(result,'<','')) as real)
+            when result = 'No Obs Odor' then null
+            else cast(result as real)
         end as quantitative_result
     from wq_data
+),
+
+final as (
+    select 
+        state_well_number,
+        sample_id,
+        sample_date,
+        analysis_method,
+        lab,
+        analyte,
+        result as raw_lab_result,
+        quantitative_result,
+        units,
+        min_detectable_limit,
+        max_report_limit as maximum_contaminant_limit,
+        row_number() over(partition by state_well_number,sample_date,analyte) as row_num
+    from results_recast
 )
 
 select 
     state_well_number,
     sample_id,
     sample_date,
-    test_method,
+    analysis_method,
     lab,
     analyte,
-    lab_result as raw_lab_result,
+    raw_lab_result,
     quantitative_result,
     units,
-    maximum_contaminant_limit,
-    lab_comment
-from results_recast
+    min_detectable_limit,
+    maximum_contaminant_limit
+from final
+where row_num = 1
