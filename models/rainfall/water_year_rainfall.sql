f66d862e1163e5ce176787fb5baaef3a354bf1e1
admin <admin@kwb.org>
2025-04-08 10:17:39 -0700

    removed hard coded table paths

--- models/rainfall/water_year_rainfall.sql
+++ models/rainfall/water_year_rainfall.sql
@@ -5,12 +5,12 @@ with
         select
             min(water_calendar_date) as water_year_start,
             max(water_calendar_date) as water_year_end
-        from dev.water_year
+        from {{ ref("water_year") }}
 
     )
 
 select gaugeid, round(sum(measurement::numeric), 2) as water_year_total
-from dev.fact_rainfall, water_year
+from {{ ref("fact_rainfall") }}, water_year
 where reading_date >= water_year_start and reading_date <= water_year_end
 group by gaugeid
 order by gaugeid
