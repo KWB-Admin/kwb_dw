1269b6b34bb639ef9130420fb184c6d2594b8101
admin <admin@kwb.org>
2025-04-11 14:25:11 -0700

    refactored to use source jinja instead of directly calling source

--- models/grazing/monthly_grazing.sql
+++ models/grazing/monthly_grazing.sql
@@ -1,10 +1,10 @@
 {{ config(materialized="view") }}
 
 with
-    max_date as (
-        select max(reporting_date) as maxdate from {{ source("agol", "fact_grazing") }}
-    )
+    grazing as (select * from {{ source("agol", "fact_grazing") }}),
 
-select agol.fact_grazing.*
-from agol.fact_grazing, max_date
+    max_date as (select max(reporting_date) as maxdate from grazing)
+
+select grazing.*
+from grazing, max_date
 where reporting_date = maxdate
