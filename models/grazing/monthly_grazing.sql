9e7b179a04dbe41d188a34703eba90c1b6aa3659
admin <admin@kwb.org>
2025-04-11 14:22:34 -0700

    ran sqlfmt

--- models/grazing/monthly_grazing.sql
+++ models/grazing/monthly_grazing.sql
@@ -1,7 +1,9 @@
 {{ config(materialized="view") }}
 
 with
-    max_date as (select max(reporting_date) as maxdate from {{ source("agol", "fact_grazing") }})
+    max_date as (
+        select max(reporting_date) as maxdate from {{ source("agol", "fact_grazing") }}
+    )
 
 select agol.fact_grazing.*
 from agol.fact_grazing, max_date
