3e8ce2c8878ff643237a25b13d021e3ddbb84997
PJ6451 <m.johnson9591@gmail.com>
2025-05-13 16:00:21 -0700

    refactored historical csv data

--- models/staging/wq/stg_historical_lab_results.sql
+++ models/staging/wq/stg_historical_lab_results.sql
@@ -4,16 +4,19 @@ with
     source as (
         select
             state_well_number,
-            sample_id,
-            sample_date,
-            test_method as analysis_method,
             lab,
+            collection_date,
+            collection_time,
             analyte,
-            lab_result as result,
+            qualifier,
+            result,
+            qualitative_result,
             units,
-            cast(null as real) as min_detectable_limit,
-            cast(maximum_contaminant_limit as real) as max_report_limit
+            minimum_reportable_limit,
+            maximum_contaminant_limit,
+            lab_sample_id
         from {{ source("water_quality", "historical_water_quality_lab_results") }}
+        where bad_data_flag is null
     )
 
 select *
